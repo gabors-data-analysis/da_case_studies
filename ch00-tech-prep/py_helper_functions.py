665cdf41294ae1820df63ebf1409a83a7ffb6493
Janos Biro <janos.biro93@gmail.com>
2020-07-14 16:38:54 +0200

    update with 08,05,07

--- ch00-tech-prep/py_helper_functions.py
+++ ch00-tech-prep/py_helper_functions.py
@@ -12,8 +12,8 @@ color = ["#3a5e8cFF", "#10a53dFF", "#541352FF", "#ffcf20FF", "#2f9aa0FF"]
 ####################################################
 # Define helper functions
 ####################################################
-def seq(start,stop,step,round_n=3):
-    return [round(x,round_n) for x in list(np.arange(start,stop,step))]
+def seq(start,stop,by,round_n=3):
+    return [round(x,round_n) for x in list(np.arange(start,stop,by))]
 
 def skew(l, round_n=3):
     return round((np.mean(l) - np.median(l)) / np.std(l), round_n)
