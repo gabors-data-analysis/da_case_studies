39169e43e4da3f5fcbf50d8132c5959787fd1ee1
Janos Biro <janos.biro93@gmail.com>
2020-08-19 17:59:28 +0200

    update ug 19

--- ch00-tech-prep/py_helper_functions.py
+++ ch00-tech-prep/py_helper_functions.py
@@ -13,37 +13,39 @@ color = ["#3a5e8cFF", "#10a53dFF", "#541352FF", "#ffcf20FF", "#2f9aa0FF"]
 ####################################################
 # Define helper functions
 ####################################################
-def seq(start,stop,by,round_n=3):
-    return [round(x,round_n) for x in list(np.arange(start,stop,by))]
+def seq(start, stop, by, round_n=3):
+    return [round(x, round_n) for x in list(np.arange(start, stop, by))]
+
 
 def skew(l, round_n=3):
     return round((np.mean(l) - np.median(l)) / np.std(l), round_n)
 
 
 def lspline(series, knots):
-    def knot_ceil(vector,knot):
+    def knot_ceil(vector, knot):
         vector_copy = copy.deepcopy(vector)
-        vector_copy[vector_copy>knot]=knot
+        vector_copy[vector_copy > knot] = knot
         return vector_copy
-    if type(knots)!=list:
-        knots=[knots]
+
+    if type(knots) != list:
+        knots = [knots]
     design_matrix = None
     vector = series.values
-    
+
     for i in range(len(knots)):
-        #print(i)
-        #print(vector)
-        if i==0:
+        # print(i)
+        # print(vector)
+        if i == 0:
             column = knot_ceil(vector, knots[i])
         else:
-            column = knot_ceil(vector, knots[i]-knots[i-1])
-        #print(column)
-        if i==0:
-            design_matrix=column
+            column = knot_ceil(vector, knots[i] - knots[i - 1])
+        # print(column)
+        if i == 0:
+            design_matrix = column
         else:
-            design_matrix = np.column_stack((design_matrix,column))
-        #print(design_matrix)
+            design_matrix = np.column_stack((design_matrix, column))
+        # print(design_matrix)
         vector = vector - column
-    design_matrix=np.column_stack((design_matrix,vector))    
-    #print(design_matrix)
+    design_matrix = np.column_stack((design_matrix, vector))
+    # print(design_matrix)
     return design_matrix
