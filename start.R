bbe806247632e15e36df11a8ee2153eafd96353f
Gábor Békés <bekesg@gmail.com>
2020-08-24 15:36:09 +0200

    Update start.R

--- start.R
+++ start.R
@@ -6,10 +6,18 @@ library(pacman)
 
 #ch00
 pacman::p_load(urca, sandwich, stargazer,stringr)
-pacman::p_load(scales, data.table )
+pacman::p_load(scales, data.table, knitr )
+
+#ch02
+pacman::p_load(lspline, cowplot)
+
 
 #ch04
 pacman::p_load(haven, Hmisc, xtable, binsreg)
 
 #ch07
 pacman::p_load(viridis, grid, gridExtra)
+
+
+#ch13-14
+pacman::p_load(lmtest, caret, glmnet, skimr, directlabels)
