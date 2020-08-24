331185d9a61ff43fd8816443bbfdf13dfee7bbe8
Gábor Békés <bekesg@gmail.com>
2020-08-24 11:31:18 +0200

    Update start.R

--- start.R
+++ start.R
@@ -1,14 +1,15 @@
 # basix
 install.packages("tidyverse")
-install.packages("here")
 install.packages("pacman")
 library(tidyverse)
 library(pacman)
 
 #ch00
-pacman::p_load(urca, sandwich, stargazer)
+pacman::p_load(urca, sandwich, stargazer,stringr)
 pacman::p_load(scales, data.table )
-
 
 #ch04
 pacman::p_load(haven, Hmisc, xtable, binsreg)
+
+#ch07
+pacman::p_load(viridis, grid, gridExtra)
