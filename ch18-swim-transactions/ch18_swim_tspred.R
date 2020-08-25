c0aff59916549087d411dc0d09fc986d84742b18
Gábor Békés <bekesg@gmail.com>
2020-08-25 17:40:42 +0200

    Update ch18_swim_tspred.R

--- ch18-swim-transactions/ch18_swim_tspred.R
+++ ch18-swim-transactions/ch18_swim_tspred.R
@@ -48,11 +48,7 @@ library(prophet)
 # option B: set working directory for da_case_studies
 #           example: setwd("C:/Users/bekes.gabor/Documents/github/da_case_studies/")
 
-# set data dir, load theme and functions
-source("ch00-tech-prep/theme_bg.R")
-source("ch00-tech-prep/da_helper_functions.R")
-
-# data used
+# set data dir, data used
 source("set-data-directory.R") #data_dir must be first defined #
 # alternative: give full path here, 
 #            example data_dir="C:/Users/bekes.gabor/Dropbox (MTA KRTK)/bekes_kezdi_textbook/da_data_repo"
@@ -63,6 +59,11 @@ use_case_dir <- "ch18-swim-transactions/"
 data_out <- use_case_dir
 output <- paste0(use_case_dir,"output/")
 create_output_if_doesnt_exist(output)
+
+
+# load theme and functions
+source("ch00-tech-prep/theme_bg.R")
+source("ch00-tech-prep/da_helper_functions.R")
 
 
 #####################################
