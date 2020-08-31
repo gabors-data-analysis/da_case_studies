372cfd6e321f558daa4a9d9d8bd060ec13b3af03
Gábor Békés <bekesg@gmail.com>
2020-08-31 09:30:52 +0200

    Update start.R
    fred dev version as cran problem

--- start.R
+++ start.R
@@ -7,18 +7,22 @@ library(pacman)
 #ch00
 pacman::p_load(urca, sandwich, stargazer,stringr)
 pacman::p_load(scales, data.table, knitr )
+install.packages("devtools")
 
 #Part I
 pacman::p_load(lspline, cowplot, arm, pastecs, DataCombine, janitor)
 pacman::p_load(haven, Hmisc, xtable, binsreg)
 
 # Part II
-pacman::p_load(viridis, grid, gridExtra)
+pacman::p_load(viridis, grid, gridExtra, dyn, estimatr, huxtable)
 
 
 #Part III
 pacman::p_load(lmtest, caret, glmnet, skimr, directlabels, 
-               prophet, timeDate, fpp3, fredr)
+               prophet, timeDate, fpp3)
+pacman::p_load(glue, vctrs) # needed for FredR
+devtools::install_github("sboysel/fredr")
+
 
 # Part IV
 p_load(MatchIt, Matching, gmodels)
