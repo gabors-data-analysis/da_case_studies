3052111403717a368ed251afe04b3c7decd09be5
Gábor Békés <bekesg@gmail.com>
2020-08-31 11:24:21 +0200

    Update start.R

--- start.R
+++ start.R
@@ -14,12 +14,12 @@ pacman::p_load(lspline, cowplot, arm, pastecs, DataCombine, janitor)
 pacman::p_load(haven, Hmisc, xtable, binsreg)
 
 # Part II
-pacman::p_load(viridis, grid, gridExtra, dyn, estimatr, huxtable)
+pacman::p_load(viridis, grid, gridExtra, dyn, estimatr, huxtable, segmented)
 
 
 #Part III
 pacman::p_load(lmtest, caret, glmnet, skimr, directlabels, 
-               prophet, timeDate, fpp3)
+               prophet, timeDate, fpp3, aTSA, plotly)
 pacman::p_load(glue, vctrs) # needed for FredR
 devtools::install_github("sboysel/fredr")
 
