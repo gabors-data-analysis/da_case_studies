c9e29a1bf4e092753fe7bb017b121506068580a4
adamvig96 <vig1396@gmail.com>
2022-10-06 17:18:44 +0200

    delete files to include in .gitignore

--- set-data-directory.R
+++
@@ -1,2 +0,0 @@
-# set-data-directory for da_data_repo!!!
-data_dir="~/Documents/Munka/Gabors_DA_book/da_case_studies"
--- set-data-directory.do
+++
@@ -1,2 +0,0 @@
-global data_dir "C:/Users/kezdi/Dropbox/bekes_kezdi_textbook/da_data_repo"
-
