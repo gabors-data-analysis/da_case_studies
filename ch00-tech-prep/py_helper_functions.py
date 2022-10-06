b8aed59e1a74427330fab3869eab9d68b8244276
adamvig96 <vig1396@gmail.com>
2022-10-06 16:08:54 +0200

    label and legend fix for plots

--- ch00-tech-prep/py_helper_functions.py
+++ ch00-tech-prep/py_helper_functions.py
@@ -17,7 +17,14 @@ color = ["#3a5e8cFF", "#10a53dFF", "#541352FF", "#ffcf20FF", "#2f9aa0FF"]
 # Define helper functions
 ####################################################
 def seq(start, stop, by, round_n=3):
-    return [round(x, round_n) for x in list(np.arange(start, stop, by))]
+    """
+    Custom function, used for setting the breaks of plotnine scales
+    """
+    epsilon = np.finfo("float").eps
+
+    return [
+        round(x, round_n) for x in list(np.arange(start, stop + (by - epsilon), by))
+    ]
 
 
 def skew(l, round_n=3):
@@ -125,3 +132,69 @@ def poly(x, degree=1):
         else:
             d[f"x**{i}"] = np.power(x, i)
     return pd.DataFrame(d)
+
+
+from sklearn.model_selection import KFold
+from statsmodels.tools.eval_measures import rmse
+import statsmodels.formula.api as smf
+
+
+def ols_crossvalidator(
+    formula: str, data: pd.DataFrame, n_folds=5, average_rmse=True
+) -> dict:
+    """OLS cross-validator
+
+
+    Estimates `formula` equation with OLS and returns values of R`2, No. coefficients,
+    BIC. Does k-fold cross-validation and either returns train and test RMSE for each
+    fold, or return averarage train and test RMSEs.
+
+    Parameters
+    ----------
+    formula : str
+        Equation that is estimated by OLS.
+    data: pd.DataFrame
+        Database in a wide format.
+    n_folds : int, default=5
+        Number of folds. Must be at least 2.
+    average_rmse: bool, default=True
+        Whether to return the average train and test RMSE of the k-fold CV, or return
+        train and test RMSE-s for each fold.
+
+    """
+
+    # Get statistics on the whole work data
+
+    model = smf.ols(formula, data=data).fit()
+
+    rsquared = model.rsquared
+    n_coefficients = model.params.shape[0]
+    bic = model.bic
+
+    # Calculating test and train RMSE-s for each fold
+
+    k = KFold(n_splits=n_folds, shuffle=False, random_state=None)
+
+    rmse_train = []
+    rmse_test = []
+
+    for train_index, test_index in k.split(data):
+
+        data_train, data_test = data.iloc[train_index, :], data.iloc[test_index, :]
+
+        model = smf.ols(formula, data=data_train).fit()
+
+        rmse_train.append(rmse(data_train["price"], model.predict(data_train)))
+        rmse_test.append(rmse(data_test["price"], model.predict(data_test)))
+
+    if average_rmse:
+        rmse_train = np.mean(rmse_train)
+        rmse_test = np.mean(rmse_test)
+
+    return {
+        "Training RMSE": rmse_train,
+        "Test RMSE": rmse_test,
+        "R-squared": rsquared,
+        "BIC": bic,
+        "Coefficients": n_coefficients,
+    }
