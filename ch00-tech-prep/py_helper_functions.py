3403e3920b1ba6f8eed2a117c75aa4cdcd6bead1
adamvig96 <vig1396@gmail.com>
2022-10-10 12:33:08 +0200

    type hints and help docu, resolve #40

--- ch00-tech-prep/py_helper_functions.py
+++ ch00-tech-prep/py_helper_functions.py
@@ -2,8 +2,10 @@
 # Import packages
 ####################################################
 import copy
+from typing import List
 
 import numpy as np
+import numpy.typing as npt
 import pandas as pd
 from plotnine import *
 
@@ -16,9 +18,20 @@ color = ["#3a5e8cFF", "#10a53dFF", "#541352FF", "#ffcf20FF", "#2f9aa0FF"]
 ####################################################
 # Define helper functions
 ####################################################
-def seq(start, stop, by, round_n=3):
+def seq(start: float, stop: float, by: float, round_n=3) -> list:
     """
-    Custom function, used for setting the breaks of plotnine scales
+    Custom function, used for setting the breaks of plotnine scales.
+
+       Parameters
+    ----------
+    start : float
+        Start of the breaks.
+    stop : float
+        End of the breaks.
+    by : float
+        Steps between breaks.
+    round_n: int, default=3
+        Decimals to round floats in output.
     """
     epsilon = np.finfo("float").eps
 
@@ -27,15 +40,28 @@ def seq(start, stop, by, round_n=3):
     ]
 
 
-def skew(l, round_n=3):
+def skew(l: npt.ArrayLike, round_n=3) -> float:
     return round((np.mean(l) - np.median(l)) / np.std(l), round_n)
 
 
-def lspline(series, knots):
-    def knot_ceil(vector, knot):
-        vector_copy = copy.deepcopy(vector)
-        vector_copy[vector_copy > knot] = knot
-        return vector_copy
+def knot_ceil(vector: np.array, knot: float) -> np.array:
+    vector_copy = copy.deepcopy(vector)
+    vector_copy[vector_copy > knot] = knot
+    return vector_copy
+
+
+def lspline(series: pd.Series, knots: List[float]) -> np.array:
+    """
+    Function to create design matrix to esitmate a piecewise
+    linear spline regression.
+
+       Parameters
+    ----------
+    series : pd.Series
+        Your variable in a pandas Series.
+    knots : List[float]
+        The knots, that result in n + 1 line segments.
+    """
 
     if type(knots) != list:
         knots = [knots]
@@ -62,14 +88,41 @@ def lspline(series, knots):
 
 
 def create_calibration_plot(
-    data,
-    file_name,
-    prob_var,
-    actual_var,
+    data: pd.DataFrame,
+    file_name: str,
+    prob_var: str,
+    actual_var: str,
     y_lab="Actual event probability",
     n_bins=10,
     breaks=None,
 ):
+    """
+    Function to create calibration curve.
+    Returns calibration curve on a plot.
+
+       Parameters
+    ----------
+    data : pd.DataFrame
+        Your dataframe, containing the actual outcome and
+        the predicted probabilities of that outcome
+        by a model.
+    file_name : str
+        Filename to save. NOTE: this is note used for now.
+    prob_var : str
+        Name of the variable, containin predicted
+        probabilities.
+    actual_var : str
+        Name of the actual outcome variable.
+    y_lab: str
+        Label on y axis of the plot.
+    n_bins : int, default=10
+        The number of bins, you would like to create.
+        This is because with many values for the predicted probabilities,
+        we may have few observations to look at for each value.
+    breaks (optional): list or None
+        You can speficy the breaks of the plot.
+    """
+
     if breaks is None:
         breaks = np.around(
             np.linspace(0, (n_bins + 1) / 10, num=n_bins + 1, endpoint=False),
@@ -88,16 +141,10 @@ def create_calibration_plot(
         .reset_index()
     )
 
-    plot = (
+    return (
         ggplot(binned_data, aes("mean_prob", "mean_actual"))
         + geom_line(color=color[0], size=1, show_legend=True)
-        + geom_point(
-            color=color[0],
-            size=1,
-            alpha=0.7,
-            show_legend=False,
-            na_rm=True,
-        )
+        + geom_point(color=color[0], size=1, alpha=0.7, show_legend=False, na_rm=True)
         + geom_segment(
             x=min(breaks),
             xend=max(breaks),
@@ -114,16 +161,21 @@ def create_calibration_plot(
         + scale_x_continuous(expand=(0.01, 0.01), breaks=(seq(0, 1.1, 0.1)))
     )
 
-    return plot
 
-
-def poly(x, degree=1):
+def poly(x: npt.ArrayLike, degree=1) -> pd.DataFrame:
     """
-    Fit Polynomial
+    Fit polynomial.
 
     These are non orthogonal factors, but it may not matter if
     we only need this for predictions (without interpreting the
     coefficients) or visualisation.
+
+    Parameters
+    ----------
+    x : npt.ArrayLike
+        Data array.
+    degree : int, default=1
+        Degree of the polynomial.
     """
     d = {}
     for i in range(degree + 1):
@@ -134,9 +186,73 @@ def poly(x, degree=1):
     return pd.DataFrame(d)
 
 
+def price_diff_by_variables(
+    df: pd.DataFrame, factor_var: str, dummy_var: str, factor_lab: str, dummy_lab: str
+) -> ggplot:
+    """
+    Price difference by selected factor and dummy variables.
+
+    This function creates a barplots looking for interactions.
+    Used in `ch14-airbnb-prediction.ipynb`.
+
+        Parameters
+    ----------
+    df : pd.DataFrame
+        Your dataframe.
+    factor_var : str
+        Your factor variable (like room_type).
+    dummy_var : str
+        The dummy variable you are interested in (like TV).
+    factor_lab : str
+        The label on the final plot for the `factor_var`.
+    dummy_lab : str
+        The label on the final plot for the `dummy_var`.
+    """
+
+    stats = df.groupby([factor_var, dummy_var]).agg(
+        Mean=("price", np.mean), sd=("price", np.std), size=("price", "size")
+    )
+    stats["se"] = stats["sd"] / stats["size"] ** (1 / 2)
+    stats["Mean_l"] = stats["Mean"] - (1.96 * stats["se"])
+    stats["Mean_u"] = stats["Mean"] + (1.96 * stats["se"])
+    stats = stats.drop(["sd", "size"], axis=1).reset_index()
+
+    return (
+        ggplot(
+            stats,
+            aes(
+                stats.columns[0],
+                stats.columns[2],
+                fill="factor(" + stats.columns[1] + ")",
+            ),
+        )
+        + geom_bar(stat="identity", position=position_dodge(width=0.9))
+        + geom_errorbar(
+            aes(ymin="Mean_l", ymax="Mean_u"),
+            position=position_dodge(width=0.9),
+            width=0.25,
+        )
+        + scale_color_manual(name=dummy_lab, values=(color[1], color[0]))
+        + scale_fill_manual(name=dummy_lab, values=(color[1], color[0]))
+        + ylab("Mean Price")
+        + xlab(factor_lab)
+        + theme_bw()
+        + theme(
+            panel_grid_major=element_blank(),
+            panel_grid_minor=element_blank(),
+            panel_border=element_blank(),
+            axis_line=element_line(),
+            legend_position="top",
+            legend_box="vertical",
+            legend_text=element_text(size=5),
+            legend_title=element_text(size=5, face="bold"),
+        )
+    )
+
+
+import statsmodels.formula.api as smf
 from sklearn.model_selection import KFold
 from statsmodels.tools.eval_measures import rmse
-import statsmodels.formula.api as smf
 
 
 def ols_crossvalidator(
@@ -153,11 +269,11 @@ def ols_crossvalidator(
     ----------
     formula : str
         Equation that is estimated by OLS.
-    data: pd.DataFrame
+    data : pd.DataFrame
         Database in a wide format.
     n_folds : int, default=5
         Number of folds. Must be at least 2.
-    average_rmse: bool, default=True
+    average_rmse : bool, default=True
         Whether to return the average train and test RMSE of the k-fold CV, or return
         train and test RMSE-s for each fold.
 
@@ -172,8 +288,10 @@ def ols_crossvalidator(
     model = smf.ols(formula, data=data).fit()
 
     rsquared = model.rsquared
-    #n_coefficients = model.params.shape[0]
-    n_coefficients = model.df_model + 1 # This might differ from model.params.shape[0], because of collinear predictors
+    # n_coefficients = model.params.shape[0]
+    n_coefficients = (
+        model.df_model + 1
+    )  # This might differ from model.params.shape[0], because of collinear predictors
     bic = model.bic
     rmse_alldata = rmse(model.predict(), data[y])
 
@@ -210,10 +328,11 @@ def ols_crossvalidator(
 import statsmodels
 
 
-def predict_interval(
+def point_predict_with_conf_int(
     regression: statsmodels.regression.linear_model.RegressionResultsWrapper,
     new_datapoint: pd.DataFrame,
     interval_precision=0.95,
+    round_n=2,
 ) -> dict:
     """
     Does point prediction and interval prediction for a new datapoint.
@@ -222,30 +341,70 @@ def predict_interval(
     ----------
     regression : statsmodels.regression.linear_model.RegressionResultsWrapper
         Fitted regression model.
-    new_datapoint: pd.DataFrame
+    new_datapoint : pd.DataFrame
         Database containing a new observation.
     interval_precision : float, default=0.95
         Precision of interval prediction.
+    round_n: int, default=2
+        Decimals to round floats in output.
     """
 
     summaryframe = regression.get_prediction(new_datapoint).summary_frame(
         alpha=1 - interval_precision
     )
 
-    point_prediction = round(summaryframe["mean"].values[0], 2)
+    point_prediction = round(summaryframe["mean"].values[0], round_n)
 
-    conf_int = (
-        "["
-        + "–".join(
-            [
-                str(round(i, 2))
-                for i in summaryframe[["obs_ci_lower", "obs_ci_upper"]].values[0]
-            ]
-        )
-        + "]"
-    )
+    conf_int = [
+        round(i, round_n)
+        for i in summaryframe[["obs_ci_lower", "obs_ci_upper"]].values[0]
+    ]
+
+    if round_n == 0:
+        point_prediction = int(point_prediction)
+        conf_int = [int(i) for i in conf_int]
+    else:
+        pass
 
     return {
         "Point prediction": point_prediction,
         f"Prediction Interval ({round(interval_precision*100)}%)": conf_int,
     }
+
+
+class Error(Exception):
+    """Base class for other exceptions"""
+
+    pass
+
+
+class ConfintError(Error):
+    """
+    Error raised when a confidence interval
+    does not match with required format.
+    """
+
+    def __init__(
+        self,
+        message="Confidence intervals are two numbers, so len(conf_int) must be 2.",
+    ):
+        self.message = message
+        super().__init__(self.message)
+
+
+def format_confidence_interval(conf_int: List[float], round_n=2) -> str:
+    """Format confidence interval.
+
+        Parameters
+    ----------
+    conf_int: np.array
+        Array, consisting upper and upper confidence interval values.
+    round_n: int, default=2
+        Decimals to round floats in output.
+    """
+    if len(conf_int) != 2:
+        raise ConfintError
+    elif round_n == 0:
+        return "[" + "–".join([str(int(round(i, round_n))) for i in conf_int]) + "]"
+    else:
+        return "[" + "–".join([str(round(i, round_n)) for i in conf_int]) + "]"
