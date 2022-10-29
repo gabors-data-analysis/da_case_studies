aa784ea34f41d25997dd4e426cef7082de5e1a81
adamvig96 <vig1396@gmail.com>
2022-10-29 12:07:53 +0200

    add 2nd axis, resolve #87

--- ch00-tech-prep/py_helper_functions.py
+++ ch00-tech-prep/py_helper_functions.py
@@ -442,3 +442,28 @@ def create_sample_frame(
         sample_frame[i] = rng.choice(vector, size=sample_size, replace=with_replacement)
 
     return sample_frame
+
+
+def add_margin(ax, x=0.05, y=0.05) -> None:
+    """
+    This will, by default, add 5% to the x and y margins to matplotlib plots.
+    You can customise this using the x and y arguments when you call it.
+
+       Parameters
+    ----------
+    ax : matplotlib.axes._subplots.AxesSubplot
+        Vector of observations.
+    x : float,default=0.05
+        Margin to add for x axis.
+    y : float,default=0.05
+        Margin to add for x axis.
+    """
+
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    xmargin = (xlim[1] - xlim[0]) * x
+    ymargin = (ylim[1] - ylim[0]) * y
+
+    ax.set_xlim(xlim[0] - xmargin, xlim[1] + xmargin)
+    ax.set_ylim(ylim[0] - ymargin, ylim[1] + ymargin)
